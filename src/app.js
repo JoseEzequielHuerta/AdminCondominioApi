1123a5fd1ae924cc779c25beb3e15d51083202ad
Jo Huerta <santiago@alimx.mx>
2022-10-04 17:08:06 -0500

    se crea cors configuracion

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 require("dotenv").config();
 
+import cors from 'cors'
 import bodyParser from "body-parser";
 import express from 'express';
 import clienteRoutes from "./routes/cliente.routes";
 import morgan from "morgan";
 
+const corsOptions ={
+    origin:process.env.FRONTEND_POINT,
+}
+
 const app = express();
+
+app.use(cors())
 
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
