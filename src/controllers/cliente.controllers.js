6d6877e15023e4b274d7f45278b84bb8143c9bbc
Jo Huerta <santiago@alimx.mx>
2022-10-04 22:51:42 -0500

    se crea terminan servicios de  cliente

--- src/controllers/cliente.controllers.js
+++ src/controllers/cliente.controllers.js
@@ -1,9 +1,59 @@
-import Cliente from "../models/Cliente";
+import Cliente from "../models/Cliente.js";
 
-exports.addClient = async (req,res) => {
-    const cliente = Cliente(req.body);
-    await cliente
-      .save(cliente)
-      .then((data)=> res.status(201).json(data))
-      .catch((err)=> res.status(501).json(err))
-}
+exports.getById = async (req, res) => {
+  const { id } = req.params;
+  await Cliente.findById(id)
+    .then((data) => res.status(200).json(data))
+    .catch((err) => res.status(501).json(err));
+};
+
+exports.getClientsActive = async (_, res) => {
+  const clientes = await Cliente.find({ done: { $eq: 1 } });
+  res.send(clientes);
+};
+
+exports.getClientsInactive = async (_, res) => {
+  const clientes = await Cliente.find({ done: { $gt: 1 } });
+  res.send(clientes);
+};
+
+exports.getClients = async (_, res) => {
+  const clientes = await Cliente.find();
+  res.send(clientes);
+};
+
+exports.addClient = async (req, res) => {
+  const cliente = Cliente(req.body);
+  await cliente
+    .save(cliente)
+    .then((data) => res.status(201).json(data))
+    .catch((err) => res.status(501).json(err));
+};
+
+exports.updateClient = async (req, res) => {
+  const { id } = req.params;
+  const cliente = req.body;
+  await Cliente.findByIdAndUpdate(id, cliente)
+    .then((data) => res.status(201).json(data))
+    .catch((err) => res.status(501).json(err));
+};
+
+exports.activeClient = async (req, res) => {
+  const { id } = req.params;
+  const cliente = await Cliente.findById(id);
+  cliente.done = 1;
+  await cliente
+    .save()
+    .then((data) => res.status(201).json(data))
+    .catch((err) => res.status(501).json(err));
+};
+
+exports.inactiveClient = async (req, res) => {
+  const { id } = req.params;
+  const cliente = await Cliente.findById(id);
+  cliente.done = 3;
+  await cliente
+    .save()
+    .then((data) => res.status(201).json(data))
+    .catch((err) => res.status(501).json(err));
+};
--- src/routes/cliente.routes.js
+++ src/routes/cliente.routes.js
@@ -1,8 +1,25 @@
 import { Router } from "express";
-import { addClient} from '../controllers/cliente.controllers';
+import {
+  getById,
+  getClientsActive,
+  getClientsInactive,
+  getClients,
+  addClient,
+  updateClient,
+  activeClient,
+  inactiveClient
+} from "../controllers/cliente.controllers";
 
 const router = Router();
 
-router.post("/add",addClient)
+router
+  .get("/get", getClients)
+  .get("/get/id=:id", getById)
+  .get("/get/active", getClientsActive)
+  .get("/get/inactive", getClientsInactive)
+  .post("/add", addClient)
+  .post("/update=:id", updateClient)
+  .post("/activeClient=:id",activeClient)
+  .post("/inactiveClient=:id",inactiveClient);
 
 export default router;
